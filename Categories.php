a005596d7989e62a963ab86a41ef0908431992ef
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-07-20 13:03:10 +0100

    Add a href linking dashboard.php & button to publish on form

--- Categories.php
+++ Categories.php
@@ -79,6 +79,16 @@
                             <label for="title"><span class="FieldInfo">Category Title:</span></label>
                             <input class="form-control" type="text" name="Title" id="title" placeholder="Type Title Here"value="">
                         </div>
+                        <div class="row" >
+                            <div class="col-lg-6 mb-2">
+                                <a href="Dashboard.php" class="btn btn-warning btn-block"><i class="fas fa-arrow-left"></i> Back to Dashboard</a>
+                            </div>
+                            <div class="col-lg-6 mb-2">
+                                <button type="button" name="Submit" class="btn btn-success btn-block">
+                                <i class="fas fa-check"></i> Publish
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </form>
