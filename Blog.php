6473b0a3ed97a87b54b288d2cd87dc173e119339
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-07-21 09:30:30 +0100

    Add the menu layout that the user will see
    The Menu includes 5 items and a search input

---
+++ Blog.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/772f078b99.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <div style="height:10px; background:#27aae1;"></div>
+    <!--Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">Paul Ffrench</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcollapseCMS" >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a href="Blog.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#.php" class="nav-link">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Blog.php.php" class="nav-link">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Features</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <form class="form-inline d-none d-sm-block" action="Blog.php" method="post">
+                    <div class="form-group">
+                        <input class="form-control mr-2" type="text" name="Search" placeholder="Search here" value="">
+                        <button type="button" class="btn btn-primary" name="SearchButton">Go</button>
+                    </div>
+
+                </form>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <div style="height:10px; background:#27aae1;"></div>
+<!-- NAVBAR END-->  
+ 
+<!-- HEADER -->
+<header class="bg-dark text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1><i class="fas fa-text-height" style="color:#27aae1;"></i>Example</h1>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- HEADER END-->
+<br>
+<!-- FOOTER -->
+<footer class="bg-dark text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <p class="lead text-center">Theme by | Jazeb Akram | <span id="year"></span> &copy; ----All rights Reserved.</p>
+            <p class="text-center small"><a style="color: white; text-decoration: none; cursor: pointer;" href="#">
+                This site is only used for studying purposes, jazebakram.com have all rights. No one is allowed to distribute copiers other than <br>
+                &trade; jazebakram.com &trade; Udemy &trade; Skillshare; &trade; StackSkills
+            </a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+<div style="height:10px; background:#27aae1;"></div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script>
+$('#year').text(new Date().getFullYear());
+
+</script>
+</body>
+</html>
