98bef294da6dec26ca504a6f5f68608c4920a406
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-07-22 12:04:17 +0100

    Add comments page template

---
+++ Comments.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comments</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/772f078b99.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <div style="height:10px; background:#27aae1;"></div>
+    <!--Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">Paul Ffrench</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcollapseCMS" >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a href="MyProfile.php" class="nav-link"><i class="fas fa-user text-success"></i> My Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Dashboard.php" class="nav-link">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Posts.php" class="nav-link">Posts</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Categories.php" class="nav-link">Categories</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Admins.php" class="nav-link">Manage Admins</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Comments.php" class="nav-link">Comments</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Blog.php" class="nav-link">Live Blog</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="Logout.php" class="nav-link"><i class="fas fa-user-times text-danger"></i> Logout </a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <div style="height:10px; background:#27aae1;"></div>
+<!-- NAVBAR END-->  
+ 
+<!-- HEADER -->
+<header class="bg-dark text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1><i class="fas fa-comments" style="color:#27aae1;"></i>Manage Comments</h1>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- HEADER END-->
+
+<!-- FOOTER -->
+<footer class="bg-dark text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <p class="lead text-center">Theme by | Jazeb Akram | <span id="year"></span> &copy; ----All rights Reserved.</p>
+            <p class="text-center small"><a style="color: white; text-decoration: none; cursor: pointer;" href="#">
+                This site is only used for studying purposes, jazebakram.com have all rights. No one is allowed to distribute copiers other than <br>
+                &trade; jazebakram.com &trade; Udemy &trade; Skillshare; &trade; StackSkills
+            </a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+<div style="height:10px; background:#27aae1;"></div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script>
+$('#year').text(new Date().getFullYear());
+
+</script>
+</body>
+</html>
