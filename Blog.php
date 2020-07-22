60135ca868622bb855c6b076def25a0cab870c4a
PaulmFfrench-dev <paul@brickfieldlabs.ie>
2020-07-22 16:36:19 +0100

    Add side content to blog page
    There is also aditional images to be used a content

--- Blog.php
+++ Blog.php
@@ -130,7 +130,7 @@
                     </li>
                     <?php } }?>
                     <?php 
-                    $ConnectionDB;
+                    $ConnectingDB;
                     $sql = "SELECT COUNT(*) FROM posts";
                     $stmt =$ConnectingDB->query($sql);
                     $RowPagination=$stmt->fetch();
@@ -168,7 +168,49 @@
         <!--MAIN AREA END -->
 
         <!--SIDE AREA START -->
-        <div class="col-sm-4" style="min-height:40px; background:green;">
+        <div class="col-sm-4" >
+            <div class="card mt-4">
+                <div class="card-body">
+                    <img src="images/startblog.png" class="d-block img-fluid mb-3" alt="">
+                    <div class="text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>                
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-header bg-dark text-light">
+                    <h2 class="lead">Sign Up!</h2>
+                </div>                
+                <div class="card-body">
+                    <button type="button" class="btn btn-success btn-block text-center text-white" name="button">Join the Forum</button>
+                    <button type="button" class="btn btn-danger btn-block text-center text-white mb-4" name="button">Login</button>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="" placeholder="Enter your email" value="">
+                        <div class="input-group-append">
+                            <button type="button" class="btn btn-primary btn-sm text-center text-white" name="button">Subscribe now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-header bg-primary text-light">
+                    <h2 class="lead">Categories</h2>
+                    </div>
+                    <div class="card-body">
+                        <?php 
+                        $ConnectingDB;
+                        $sql = "SELECT * FROM category ORDER BY id desc";
+                        $stmt = $ConnectingDB->query($sql);
+                        while($DataRows=$stmt->fetch()){
+                            $CategoryId = $DataRows["id"];
+                            $CategoryName = $DataRows["title"];
+                        ?>
+                        <a href="Blog.php?category=<?php echo $CategoryName; ?>"><span class="heading"><?php echo $CategoryName; ?><br></span></a>
+                        <?php }?>
+                    </div>
+            </div>
         </div>
         <!--SIDE AREA END -->
     </div>
